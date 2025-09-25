a81a1c2b33358329937472af4de07edbcbdd4199
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-09-25 14:56:49 +0900

    chore: drop zx dependency

--- packages/core/example/example.js
+++ packages/core/example/example.js
@@ -1,5 +1,39 @@
-import { $ } from "zx";
+import { mkdir, writeFile } from "node:fs/promises";
+import { spawn } from "node:child_process";
 
-await $`mkdir -p latexjs webtex`;
-await $`npx latex.js --pretty example.tex --assets=latexjs > latexjs/index.html`;
-await $`node ../bin/webtex --pretty example.tex --assets=webtex > webtex/index.html`;
+async function run(command, args) {
+  return new Promise((resolve, reject) => {
+    const child = spawn(command, args, { stdio: ["ignore", "pipe", "inherit"] });
+    const chunks = [];
+    child.stdout.on("data", (data) => {
+      chunks.push(data);
+    });
+    child.on("error", reject);
+    child.on("exit", (code) => {
+      if (code === 0) {
+        resolve(Buffer.concat(chunks).toString("utf8"));
+      } else {
+        reject(new Error(`${command} exited with code ${code}`));
+      }
+    });
+  });
+}
+
+await mkdir("latexjs", { recursive: true });
+await mkdir("webtex", { recursive: true });
+
+const latexJsOutput = await run("npx", [
+  "latex.js",
+  "--pretty",
+  "example.tex",
+  "--assets=latexjs",
+]);
+await writeFile("latexjs/index.html", latexJsOutput, "utf8");
+
+const webtexOutput = await run("node", [
+  "../bin/webtex",
+  "--pretty",
+  "example.tex",
+  "--assets=webtex",
+]);
+await writeFile("webtex/index.html", webtexOutput, "utf8");
